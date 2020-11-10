256f9c2eb9134439cd87c2b14a80f81853fbcb7d
Rohit Sawai <rohitsawai2893@gmail.com>
2020-11-10 09:41:01 +0530

    read raw of github using https

--- juggling-async.js
+++ juggling-async.js
@@ -4,8 +4,8 @@ const url_1 = process.argv[2];
 const url_2 = process.argv[3];
 const url_3 = process.argv[4];
 
-async function jugAsyncRes(url) {
-  await jugAsync(url, callback);
+function jugAsyncRes(url) {
+  return jugAsync(url, callback);
 }
 
 function callback(data) {
